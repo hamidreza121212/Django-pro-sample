30bdc61524ed58f05fcb7737c8d11653f99e9585
hamidreza <www.hamidreza121212@gmail.com>
2021-06-29 14:49:52 +0430

    Update admin.py
    update admin.py

--- config/blog/admin.py
+++ config/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Article, Category
-# from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
 # Register your models here.
 
